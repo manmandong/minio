1556d048f6d402e25f47f0b35cac449c6b21a680
manmandong <2417669132@qq.com>
2019-06-05 13:37:54 +0000

    Update signature-v4-parser.go
    7days to 20 years

--- cmd/signature-v4-parser.go
+++ cmd/signature-v4-parser.go
@@ -224,7 +224,7 @@ func parsePreSignV4(query url.Values, region string, stype serviceType) (psv pre
 	}
 
 	// Check if Expiry time is less than 7 days (value in seconds).
-	if preSignV4Values.Expires.Seconds() > 604800 {
+	if preSignV4Values.Expires.Seconds() > 630720000 {
 		return psv, ErrMaximumExpires
 	}
 
